ccdb819d44d0eb5edec2ce02ba644a091f59f790
joao victor <joaovictormdasilva676@gmail.com>
2023-10-08 12:12:52 -0300

    Seller entity class

--- DAO-JDBC/src/application/Program.java
+++ DAO-JDBC/src/application/Program.java
@@ -1,13 +1,26 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 		Department dp = new Department(1, "Books");
-		
 		System.out.println(dp);
+
+		try {
+			Seller seller = new Seller(1, "João Victor", "joao@gmail.com", sdf.parse("16/03/2000"), 2000.0, dp);
+			System.out.println(seller);
+		} catch (ParseException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 
 }

e7f458d9ff64f7b1aa4f98a69632d63005af3b11
joao victor <joaovictormdasilva676@gmail.com>
2023-10-15 17:44:35 -0300

    insert department

--- DAO-JDBC/src/application/Program.java
+++ DAO-JDBC/src/application/Program.java
@@ -1,53 +1,15 @@
 package application;
 
-import java.text.SimpleDateFormat;
-import java.util.List;
-
 import model.dao.DaoFactory;
-import model.dao.SellerDao;
+import model.dao.generics.IGenercs;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		IGenercs<Department> depGenercs = DaoFactory.createDepartmentDao();
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-		SellerDao sellerDao = DaoFactory.creteSellerDao();
-
-		Seller seller = sellerDao.findById(3);
-
-//		System.out.println(seller);
-
-		Department dp = new Department(2, null);
-		List<Seller> sellers = sellerDao.findByDepartment(dp);
-
-//		sellers.forEach(System.out::println);
-
-		sellers = sellerDao.findAll();
-
-//		sellers.forEach(System.out::println);
-
-		// INSERT SELLER;
-
-//		try {
-//			Seller sellerInsert = new Seller(null, "João Victor", "joaovictor@gmail.com", sdf.parse("16/03/2000"), 3500.00, dp);
-//			sellerDao.insert(sellerInsert);
-//		} catch (ParseException e) {
-//			System.out.println(e.getMessage());
-//		}
-//		
-//		sellers.forEach(System.out::println);
-
-		seller = sellerDao.findById(9);
-		seller.setName("Thalita");
-		seller.setEmail("thalita@gmail.com");
-		sellerDao.update(seller);
-
-		sellerDao.deleteById(9);
-		sellers.forEach(System.out::println);
-
+		depGenercs.insert(new Department(5, "GAMES"));
 	}
 
 }
--- DAO-JDBC/src/model/dao/impl/DepartmentDaoJDBC.java
+++ DAO-JDBC/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,12 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.dao.impl.genercs.RepositoryGenerics;
 import model.entities.Department;
@@ -21,20 +25,37 @@ public class DepartmentDaoJDBC extends RepositoryGenerics<Department> implements
 
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement ps = null;
+
+		try {
+			ps = conn.prepareStatement("INSERT INTO department (Id, Name) VALUES (?, ?)");
+			ps.setInt(1, obj.getId());
+			ps.setString(2, obj.getName());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					throw new DbIntegrityException(e.getMessage());
+				}
+			}
+		}
+
 	}
 
 	@Override
 	public void update(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(int id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
